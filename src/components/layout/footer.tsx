a745f097df20c299842a3ae7a010056114c6d1c0
Waqas Ahmad <hashirwaqas909@gmail.com>
2025-11-13 14:40:33 +0000

    the position is not setted the tktok button is behind the whatsapp butto

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -26,8 +26,8 @@ export default function Footer() {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer className="bg-card">
-      <div className="container mx-auto flex flex-col items-center justify-between gap-4 py-10 md:h-24 md:flex-row md:py-0">
+    <footer className="bg-card border-t">
+      <div className="container mx-auto flex flex-col items-center justify-between gap-4 py-10 md:h-24 md:flex-row md:py-0 pb-28 md:pb-10">
         <div className="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
           <Icons.logo className="h-6 w-6 text-primary" />
           <p className="text-center text-sm leading-loose text-muted-foreground md:text-left">
